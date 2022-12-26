91d144ce99fdfe04e4836181797bed0c4c7fcfda
Daky Chanthavong <daki.litd@gmail.com>
2022-12-26 22:46:57 +0700

    Declare environment variables

--- server.go
+++ server.go
@@ -9,6 +9,18 @@ import (
 	emdw "github.com/labstack/echo/v4/middleware"
 )
 
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+var (
+	PORT   = GetEnv("PORT", "2565")
+	PG_URL = os.Getenv("DATABASE_URL")
+)
+
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
