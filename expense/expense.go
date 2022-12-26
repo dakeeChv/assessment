39e068a86431e7fd0ed874fa683d05c803013d97
Daky Chanthavong <daki.litd@gmail.com>
2022-12-26 19:12:45 +0700

    Change id of expense to integer and add created method for new expense

--- expense/expense.go
+++ expense/expense.go
@@ -1,9 +1,35 @@
 package expense
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
 type Expense struct {
-	ID     string   `json:"id"`
+	ID     int64    `json:"id"`
 	Title  string   `json:"title"`
 	Amount float64  `json:"amount"`
 	Note   string   `json:"note"`
 	Tags   []string `json:"tags"`
+}
+
+type Service struct {
+	db *sql.DB
+}
+
+func (s *Service) Create(ctx context.Context, in Expense) (Expense, error) {
+	stmt, err := s.db.PrepareContext(ctx, `INSERT INTO expenses(title, amount, note, tags) VALUES($1, $2, $3, $4)`)
+	if err != nil {
+		return Expense{}, fmt.Errorf("Create(): db prepare context failure: %w", err)
+	}
+	result, err := stmt.ExecContext(ctx, in.Title, in.Amount, in.Note, in.Tags)
+	if err != nil {
+		return Expense{}, fmt.Errorf("Create(): db exec statement failure: %w", err)
+	}
+
+	out := in
+	out.ID, _ = result.LastInsertId()
+
+	return out, nil
 }
