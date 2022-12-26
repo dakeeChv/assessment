0686826376528f58cb965b87cbccbeae4cedff52
Daky Chanthavong <daki.litd@gmail.com>
2022-12-26 22:46:17 +0700

    Make the new echo server function

--- server.go
+++ server.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	emdw "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+}
+
+func newEchoServer() *echo.Echo {
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(
+		emdw.Logger(),
+		emdw.Recover(),
+		emdw.CORS(),
+		emdw.Secure(),
+	)
+	e.GET("/health", func(c echo.Context) error { return c.NoContent(http.StatusOK) })
+	return e
 }
