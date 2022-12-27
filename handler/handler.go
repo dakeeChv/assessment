6fbf37208c2fdf50388c2299608149bc1ee5cc8e
Daky Chanthavong <daki.litd@gmail.com>
2022-12-27 22:23:34 +0700

    Make the custom middleware

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 
@@ -21,6 +23,25 @@ func NewHandler(_ context.Context, expense *expn.Service) (*Handler, error) {
 }
 
 func (h *Handler) SetupRoute(e *echo.Echo) {
+	// Sample authentication with pare data value.
+	cmdw := func() echo.MiddlewareFunc {
+		return func(next echo.HandlerFunc) echo.HandlerFunc {
+			return func(c echo.Context) (err error) {
+				val := c.Request().Header.Get(echo.HeaderAuthorization)
+				_, err = time.Parse("January 02, 2006", val)
+				if err == nil {
+					next(c)
+				}
+				return &echo.HTTPError{
+					Code:     http.StatusUnauthorized,
+					Message:  "Unauthorized",
+					Internal: err,
+				}
+			}
+		}
+	}
+
 	v1 := e.Group("")
+	v1.Use(cmdw())
 	v1.POST("/expenses", h.CreateExpense)
 }
