d9b62573f28bf62eed21cebf732f9a0b29bbd331
Daky Chanthavong <daki.litd@gmail.com>
2022-12-28 15:39:08 +0700

    Add expense to 2 record for integration test

--- db/000001_init_schema.up.sql
+++ db/000001_init_schema.up.sql
@@ -6,4 +6,7 @@ CREATE TABLE IF NOT EXISTS expenses (
   tags TEXT[]
 );
 
-INSERT INTO expenses(id, title, amount, note, tags) VALUES(121,'watermelon',54,'Big C promotion discount 9 bath','{"food","beverage"}')
+INSERT INTO expenses(id, title, amount, note, tags)
+  VALUES
+    (121,'watermelon',54,'Big C promotion discount 9 bath','{"food","beverage"}'),
+    (123, 'iPhone 14 Pro Max 1TB', 66900, 'birthday gift from my love', '{"gadget"}')
